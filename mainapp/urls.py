92cd49677012a0004962c8297cca6e3a9a7d7542
Nandakishore-pr <nandakishore.p.r2002@gmail.com>
2025-01-28 14:52:23 +0530

    task-829 authentication completed

---
+++ mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout_user'),
+    path('regitster/',views.register_user,name = 'register_user')
+]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, 'you have looged in')
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('home')
+    else:
+        return render(request, 'home.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'you have logged out')
+    return redirect('home')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You have registered successfully.')
+                return redirect('home')
+            else:
+                messages.error(request, 'Authentication failed.')
+                return redirect('register_user')
+        else:
+            # If the form is not valid, render the template again with the invalid form
+            messages.error(request, 'Please fix the errors below.')
+            return render(request, 'register_user.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'register_user.html', {'form': form})
